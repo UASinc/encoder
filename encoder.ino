e018d5cbeb5b148af4835e01667b823f689ca3ca
UASinc <56030219+UASinc@users.noreply.github.com>
2021-03-14 11:11:08 -0400

    Update encoder.ino

--- encoder.ino
+++ encoder.ino
@@ -1,17 +1,24 @@
-const int encoder_a = 3; // Pin 3
-const int encoder_b = 2; // Pin 5
-const int hysteresis = 5; //hysteresis 5 readings, sensitivity 2mm ((wheelDia*PI)/600)*hysteresis
-const int pi = 3.14;
+/*
+ * STEVE PATTERSON
+ * UAS INC. 
+ * 
+ * ARDUINO NANO
+ * LPD3806-600BM-G5-24C
+ * 
+ * DEVICE TO MEASURE DISTANCE AND SPEED
+ */
 
+
+const int encoder_a = 3; // Pin 3 GREEN
+const int encoder_b = 2; // Pin 2 WHITE
+const int hysteresis = 5; //hysteresis 5 readings, sensitivity 2mm ((wheelDia*PI)/600)*hysteresis
+const float wheelDia = 55.0;  //wheel diameter in mm
+const float singleTick = (wheelDia*PI)/600; //one tick lenght in mm
 volatile long flag_A = 0;  //Assign a value to the token bit
 volatile long encoder_pulse_counter = 0;
 volatile long direction = 1;
 volatile int mps = 0;
-
-const float wheelDia = 50.0;  //wheel diameter in mm
-const float singleTick = (wheelDia*PI)/600; //one tick lenght in mm
 short count = 0; // unsigned long keytime = 0;
-
 
 
 void measure() // Interrupt function
@@ -62,10 +69,10 @@ void loop()
 void displayUnits(long ticks) 
 {
       long speed = encoder_pulse_counter/600.00*60; // For encoder plate with 600 Pulses per Revolution
-      mps = ( speed * pi * wheelDia ) / 60;
-      Serial.print(float((float(ticks)/1000)*singleTick),3);
+      mps = ( speed * PI * wheelDia ) / 60;
+      Serial.print(float((float(ticks)/1000)*singleTick),2);
       Serial.print(" meters  |  ");
-      Serial.print(ticks*singleTick/25.4/12);
+      Serial.print((ticks*singleTick/25.4/12),2);
       Serial.print(" feet  |  ");
       Serial.print(direction*speed,DEC);
       Serial.print(" RPM  |  ");
